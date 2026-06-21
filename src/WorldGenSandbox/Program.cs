069097d1f97a0433243169149bede2a7d06c3931
Nuclear Chaos <ethan.d.schwab@gmail.com>
2026-06-20 23:36:46 -0600

    oh wow thats small

--- src/WorldGenSandbox/Program.cs
+++ src/WorldGenSandbox/Program.cs
@@ -1,12 +1,1 @@
-using System;
-using WorldGenSandbox;
-
-internal partial class Program
-{
-    [STAThread]
-    private static void Main(string[] args)
-    {
-        using var game = new Game1();
-        game.Run();
-    }
-}
+new WorldGenSandbox.Game1().Run();
