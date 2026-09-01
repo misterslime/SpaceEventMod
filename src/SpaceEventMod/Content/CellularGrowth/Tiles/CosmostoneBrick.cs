51d6f8b83d4d1560f1396d7ff0f5fc35bb1bcb6a
Nuclear Chaos <ethan.d.schwab@gmail.com>
2026-08-31 23:27:21 -0600

    tile merging

--- src/SpaceEventMod/Content/CellularGrowth/Tiles/CosmostoneBrick.cs
+++ src/SpaceEventMod/Content/CellularGrowth/Tiles/CosmostoneBrick.cs
@@ -20,6 +20,15 @@ internal class CosmostoneBrick : ModTile, ILoadItem
         Main.tileSolid[Type] = true;
         Main.tileBlockLight[Type] = true;
         Main.tileBrick[Type] = true;
+        Main.tileMerge[ModContent.TileType<Cosmostone>()][Type] = true;
+        Main.tileMerge[Type][ModContent.TileType<Cosmostone>()] = true;
+        Main.tileMerge[ModContent.TileType<HerbCell>()][Type] = true;
+        Main.tileMerge[Type][ModContent.TileType<HerbCell>()] = true;
+        Main.tileMerge[ModContent.TileType<Cosmoss>()][Type] = true;
+        Main.tileMerge[Type][ModContent.TileType<Cosmoss>()] = true;
+        Main.tileBlendAll[Type] = true;
+
+        TileID.Sets.ChecksForMerge[Type] = true;
 
         AddMapEntry(new Color(62, 56, 80));
 
